0901b45687da2dbdeb16b113577812ae62e1baef
tengjianyujiang-design <tengjianyujiang@gmail.com>
2026-06-12 01:08:54 +0900

    Create app.py

---
+++ schedule-app/app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from fetch_schedule import fetch_schedule
+from send_line import send_line_message
+
+app = FastAPI()
+
+# templates フォルダを指定
+templates = Jinja2Templates(directory="templates")
+
+# static フォルダを指定
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    schedule = fetch_schedule()
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "schedule": schedule}
+    )
+
+
+@app.post("/notify")
+def notify():
+    schedule = fetch_schedule()
+    send_line_message(schedule)
+    return {"status": "ok", "message": "通知を送信しました"}
