bda5021e5853ba52b27d39a6bbe3e32fca35068c
tengjianyujiang-design <tengjianyujiang@gmail.com>
2026-06-14 21:46:58 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+
+LINE_NOTIFY_TOKEN = "XD8K1pZBHpYxQmOVyrtEVjq3wTJFOTkOx0vH/iWQxJXA/5H+gmX+/bS/PBcIR505yL3w8tp1s+awNTJu/atY6FWcp+2/jaMiEIxw5yUvcGs3UdrWhzWygszNMPtHCX+uhtqjEsVO22gAlNvHHefhJwdB04t89/1O/w1cDnyilFU="
