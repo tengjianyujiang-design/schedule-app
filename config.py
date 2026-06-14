a3d9e1dbd942d73f95cb5ee14c52774470a8c138
tengjianyujiang-design <tengjianyujiang@gmail.com>
2026-06-14 22:43:01 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # config.py
+import os
 
-LINE_NOTIFY_TOKEN = "XD8K1pZBHpYxQmOVyrtEVjq3wTJFOTkOx0vH/iWQxJXA/5H+gmX+/bS/PBcIR505yL3w8tp1s+awNTJu/atY6FWcp+2/jaMiEIxw5yUvcGs3UdrWhzWygszNMPtHCX+uhtqjEsVO22gAlNvHHefhJwdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("XD8K1pZBHpYxQmOVyrtEVjq3wTJFOTkOx0vH/iWQxJXA/5H+gmX+/bS/PBcIR505yL3w8tp1s+awNTJu/atY6FWcp+2/jaMiEIxw5yUvcGs3UdrWhzWygszNMPtHCX+uhtqjEsVO22gAlNvHHefhJwdB04t89/1O/w1cDnyilFU=")
+LINE_USER_ID = os.environ.get("U3c7003a861e2207ad31e77f3b57d7967")
